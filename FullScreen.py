913eeb1c603d65dbf538f92e39c1dffb7f76eb99
Jun Funada <jun2@alt-area.flop.jp>
2013-05-14 23:20:00 +0900

    Display in Full-Screen
    If press ESC key, release Full-Screen mode.
    If press ESC again, display in Full-Screen mode.

--- FullScreen.py
+++ FullScreen.py
@@ -2,9 +2,26 @@ import sys
 from PySide import QtCore
 from PySide import QtGui
 
-app = QtGui.QApplication(sys.argv)
-pixmap = QtGui.QPixmap('hoge.jpg')
-window = QtGui.QLabel()
-window.setPixmap(pixmap)
-window.show()
-sys.exit(app.exec_())
+
+class Screen(QtGui.QLabel):
+    def __init__(self, parent=None, f=0):
+        QtGui.QLabel.__init__(self, parent, f)
+
+    def keyPressEvent(self, event):
+        QtGui.QLabel.keyPressEvent(self, event)
+        if event.key() == QtCore.Qt.Key_Escape:
+            print(event.key())
+            if self.isFullScreen():
+                self.showNormal()
+                self.showMaximized()
+            else:
+                self.showFullScreen()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    pixmap = QtGui.QPixmap('hoge.jpg')
+    screen = Screen()
+    screen.setPixmap(pixmap)
+    screen.showFullScreen()
+    sys.exit(app.exec_())
